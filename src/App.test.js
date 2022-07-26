a8eed51d8bb702cc40c8789fa5d705954d6103c7
Nik Krimm <nik@thoughtful.ai>
2022-07-26 07:35:35 -0500

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Displays greeting', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Renders in dark-mode by default', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
